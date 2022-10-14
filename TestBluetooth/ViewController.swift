75b50ce5a8f4adcf5e7cf6229d4de68bb8bc0ec2
naoyuki.kan <naoyuki.kan@yappli.co.jp>
2022-10-14 11:03:52 +0900

    最小限の権限確認用のプロジェクトを修正

--- TestBluetooth/ViewController.swift
+++ TestBluetooth/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
+    var centralManager: CBCentralManager?
+    var peripheral: CBPeripheral?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
-
 }
 
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("Bluetoothの設定は", central.state)
+        switch central.state {
+        case .unauthorized:
+            switch central.authorization {
+            case .allowedAlways: break
+            case .denied: break
+            case .restricted: break
+            case .notDetermined: break
+            @unknown default:
+                fatalError()
+            }
+        case .unknown: break
+        case .unsupported:  break
+        case .poweredOn:
+            self.centralManager?.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey:true])
+        case .poweredOff: break
+        case .resetting: break
+        @unknown default:
+            print("知らない")
+        }
+    }
+}
